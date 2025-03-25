21b81f6bc2dccc984ebf56ef51763b7ffd18fd56
Dmitrii Gusev <amidg4x4dima@gmail.com>
2025-03-24 19:16:23 -0700

    compiling hardware interface with libcreate

--- hardware_interfaces/include/hardware_interfaces/roomba_control.hpp
+++ hardware_interfaces/include/hardware_interfaces/roomba_control.hpp
@@ -1,17 +1,3 @@
-// Copyright 2021 ros2_control Development Team
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 #ifndef ROS2_CONTROL_DEMO_EXAMPLE_2__DIFFBOT_SYSTEM_HPP_
 #define ROS2_CONTROL_DEMO_EXAMPLE_2__DIFFBOT_SYSTEM_HPP_
 
@@ -29,6 +15,9 @@
 #include "rclcpp/time.hpp"
 #include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
 #include "rclcpp_lifecycle/state.hpp"
+
+// roomba stuff
+#include "create/create.h"
 
 namespace ros2_control_demo_example_2
 {
@@ -59,6 +48,8 @@ private:
   // Parameters for the DiffBot simulation
   double hw_start_sec_;
   double hw_stop_sec_;
+
+  create::RobotModel model = create::RobotModel::CREATE_2;
 };
 
 }  // namespace ros2_control_demo_example_2
